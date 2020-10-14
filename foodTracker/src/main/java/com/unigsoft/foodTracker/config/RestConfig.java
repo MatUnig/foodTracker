8f0ddfacfcb12c2de5ce7ecefdf5915a145087a8
MatUnig <uniatowski.mateusz@gmail.com>
2020-10-14 10:47:56 +0200

    Backend commit

---
+++ foodTracker/src/main/java/com/unigsoft/foodTracker/config/RestConfig.java
@@ -0,0 +1,30 @@
+package com.unigsoft.foodTracker.config;
+
+import com.unigsoft.foodTracker.dao.ProductRepository;
+import com.unigsoft.foodTracker.entity.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        // Disabling HTTP methods Delete,Put,Post for Product and ProductCategories
+        HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.POST};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductRepository.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+
+    }
+}
